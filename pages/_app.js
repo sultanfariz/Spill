61177be47daf6739c8166f86421fd4f5c95fe2ae
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2021-11-20 18:16:04 +0700

    chore: add emotion cache config

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,20 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
-import Head from 'next/head'
-import { ThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
-import { CacheProvider } from '@emotion/react'
-import theme from '../styles/theme'
-import { createEmotionCache } from '../src/utils/createEmotionCache'
-import DateAdapter from '@mui/lab/AdapterDateFns'
-import LocalizationProvider from '@mui/lab/LocalizationProvider'
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../styles/theme';
+import { createEmotionCache } from '../src/utils/createEmotionCache';
+import DateAdapter from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import { RootLayout } from '../src/components/Layout/RootLayout';
 
 // Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache()
+const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
   return (
     <CacheProvider value={emotionCache}>
@@ -25,15 +26,17 @@ export default function MyApp(props) {
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <LocalizationProvider dateAdapter={DateAdapter}>
-          <Component {...pageProps} />
+          <RootLayout>
+            <Component {...pageProps} />
+          </RootLayout>
         </LocalizationProvider>
       </ThemeProvider>
     </CacheProvider>
-  )
+  );
 }
 
 MyApp.propTypes = {
   Component: PropTypes.elementType.isRequired,
   emotionCache: PropTypes.object,
   pageProps: PropTypes.object.isRequired,
-}
+};
